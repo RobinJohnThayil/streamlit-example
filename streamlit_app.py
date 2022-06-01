c8d1ac0220becb958c98b5dda4a4691a4792008b
RobinJohnThayil <robinjon@gmail.com>
2022-06-01 23:05:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,16 +3,16 @@ import streamlit.components.v1 as components
 import requests
 import pandas as pd
 
-# def theTweet(tweet_url):
-#     api = "https://publish.twitter.com/oembed?url={}".format(tweet_url)
-#     response = requests.get(api)
-#     res = response.json()["html"]
-#     return res
+def theTweet(tweet_url):
+    api = "https://publish.twitter.com/oembed?url={}".format(tweet_url)
+    response = requests.get(api)
+    res = response.json()["html"]
+    return res
 
 # input = st.text_input("Enter your tweet url")
 
 
-res = theTweet(https://twitter.com/SingAjai/status/1532044961604313088)
+res = theTweet("https://twitter.com/SingAjai/status/1532044961604313088")
 st.write(res)
 components.html(res,height= 700)
     
