4806d42b2ad0d86024e8cfc5e51e29c810847292
RobinJohnThayil <robinjon@gmail.com>
2022-06-01 23:06:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ def theTweet(tweet_url):
 # input = st.text_input("Enter your tweet url")
 
 
-res = theTweet("https://twitter.com/SingAjai/status/1532044961604313088")
+res = theTweet(https://twitter.com/SingAjai/status/1532044961604313088)
 st.write(res)
 components.html(res,height= 700)
     
