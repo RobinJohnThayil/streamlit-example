a05328113b7eb5b028f0afeaa6dab6783b714ca9
RobinJohnThayil <robinjon@gmail.com>
2022-06-01 22:32:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,3 +13,12 @@ if input:
     res = theTweet(input)
     st.write(res)
     components.html(res,height= 700)
+    
+d = {
+    "one": pd.Series([1.0, 2.0, 3.0], index=["a", "b", "c"]),
+    "two": pd.Series([1.0, 2.0, 3.0, 4.0], index=["a", "b", "c", "d"]),
+}
+    
+st.table(data=d)
+
+
