e89e6ac7f398c1991f78249ba43b5453013afbbd
RobinJohnThayil <robinjon@gmail.com>
2022-06-02 13:24:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,8 +30,8 @@ def display_tweets():
 
 
 d = {
-    "one": pd.Series([1.0, 2.0, 3.0], index=["a", "b", "c"]),
-    "two": pd.Series([1.0, 2.0, 3.0, 4.0], index=["a", "b", "c", "d"]),
+    "Topic": pd.Series([1]),
+    "High Fequency Words": pd.Series(["new, know, time"]),
 }
  
 e = pd.DataFrame(d)
